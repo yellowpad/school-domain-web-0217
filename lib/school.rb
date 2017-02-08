5b29e8a71a70ef09182a763a3587df9a80cff7c7
dgwp-projects <diggable.co@gmail.com>
2017-02-08 17:48:36 -0500

    learn commits

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
+class School
+	attr_accessor(:name, :roster)
+
+	def initialize(school)
+		@school = school
+		@roster = {}
+	end
+	
+	def add_student(student_name,grade)
+		@roster[grade] ||= [] 
+		@roster[grade ] << student_name
+	end
+
+	def grade(students_grade)
+		@roster[students_grade]
+	end
+
+	def sort
+		
+		sorted = {}
+		
+		@roster.each do |grade,student|
+			@roster[grade] = student.sort
+		end
+	end
+end
+
 # code here!
